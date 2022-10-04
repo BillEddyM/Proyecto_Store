e20261298ca3eb1846573d25b20c75a9b1ca715e
BillEddyM <billeddydaniel2018@gmail.com>
2022-10-04 16:20:09 -0600

    ventas, reportes

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\bille\Documents\Semestre 8\Proyoecto_SI2_VentaTennis\env
+set VIRTUAL_ENV=C:\Users\bille\Documents\Semestre 8\Proyecto_Store\env
 
 if not defined PROMPT set PROMPT=$P$G
 
